3180236b21f2b2cf320ff6d9a23b02635e63fdde
Chase A <99326567+ActuallyChaseyy@users.noreply.github.com>
2022-10-08 22:53:51 +1100

    Added basic error handling

--- tixtepy.py
+++ tixtepy.py
@@ -1,8 +1,9 @@
+from shutil import ExecError
 import requests
 
 url = "https://api.tixte.com/v1/"
 
-class tixtepy:
+class tixtepy():
 
     global hasInitialised
     hasInitialised = False;
@@ -35,69 +36,80 @@ class tixtepy:
         
         print("Fetching..")
 
-        r = requests.get(url + "users/@me/domains", headers=data)
+        try:
+            r = requests.get(url + "users/@me/domains", headers=data)
 
-        print(r.text)
+            print(r.text)
 
-        resJSON = r.json()
-        res = resJSON["data"]
+            resJSON = r.json()
+            res = resJSON["data"]
 
-        return res["domains"]
+            return res["domains"]
+        
+        except Exception:
+            return Exception
 
     def GetDomainInfo(domain):
         if not hasInitialised:
             print("No api key or authorization provided!\nPlease run tixtepy.init(\"apiKey\" \"authKey\") first!")
             return
+        try:
+            r = requests.get(url + "users/@me/domains", headers=data)
 
-        r = requests.get(url + "users/@me/domains", headers=data)
-        
-        rJSON = r.json()
+            rJSON = r.json()
 
-        response = rJSON['data']
-        domains = response['domains']
+            response = rJSON['data']
+            domains = response['domains']
 
-        for object in domains:
-            if not object['name'] == domain:
-                continue
-            else:
-                return object
+            for object in domains:
+                if not object['name'] == domain:
+                    continue
+                else:
+                    return object
+        except Exception:
+            return Exception
 
     def GetAccountInfo():
         if not hasInitialised:
             print("No api key or authorization provided!\nPlease run tixtepy.init(\"apiKey\" \"authKey\") first!")
             return
-        
-        r = requests.get(url + "users/@me/uploads/size", headers=data)
+        try:
+            r = requests.get(url + "users/@me/uploads/size", headers=data)
 
-        resJSON = r.json()
-        resData = resJSON['data']
+            resJSON = r.json()
+            resData = resJSON['data']
 
-        AccountLevel = resData['premium_tier']
+            premium_tier = resData['premium_tier']
 
-        if AccountLevel == 0:
-            UploadLimit = 15
-        elif AccountLevel == 1:
-            UploadLimit = 200
-        elif AccountLevel == 2:
-            UploadLimit = 500
+            if premium_tier == 0:
+                UploadLimit = 15
+            elif premium_tier == 1:
+                UploadLimit = 200
+            elif premium_tier == 2:
+                UploadLimit = 500
 
-        usedBytes = resData['used']
-        usedGb = round(float(usedBytes / 1000000000), 2)
+            usedBytes = resData['used']
+            usedGb = round(float(usedBytes / 1000000000), 2)
 
-        return [{'AccountLevel': AccountLevel, 'UploadLimit': UploadLimit, 'UsedSpace': usedGb}]
+            return [{'AccountLevel': premium_tier, 'UploadLimit': UploadLimit, 'UsedSpace': usedGb}]
+        except Exception:
+            return Exception
 
     def DeleteDomain(domain):
         if not hasInitialised:
             print("No api key or authorization provided!\nPlease run tixtepy.init(\"apiKey\" \"authKey\") first!")
             return
         
-        r = requests.delete(url + "users/@me/domains/" + domain, headers = data)
+        try:
+            r = requests.delete(url + "users/@me/domains/" + domain, headers = data)
 
-        rJSON = r.json()
-        status = rJSON['success']
+            rJSON = r.json()
+            status = rJSON['success']
 
-        if not status == True:
-            print("Could not delete domain, likely doesn't exist")
-            return
+            if not status == True:
+                print("Could not delete domain, likely doesn't exist")
+                return
 
-        return rJSON['data']
+            return rJSON['data']
+        except Exception:
+            return Exception
